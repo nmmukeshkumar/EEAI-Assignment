b610c06dbf62c0b1adf83df487f7382724c627fe
nmmukeshkumar <nmmukeshkumar@gmail.com>
2024-04-25 19:21:37 +0100

    Updated the predict and print code to inculcate multi label classification. Have also edited the way to print results

--- model/randomforest.py
+++ model/randomforest.py
@@ -38,11 +38,34 @@ class RandomForest(BaseModel):
         self.mdl = self.mdl.fit(data.X_train, data.y_train)
 
     def predict(self, X_test: pd.Series):
+        print("Evaluating")
         predictions = self.mdl.predict(X_test)
         self.predictions = predictions
 
     def print_results(self, data):
-        print(classification_report(data.y_test, self.predictions))
+        data.y_test = data.y_test.to_numpy()
+        print("Printing Results")
+        induvidual_accuracies = []
+        array_length = len(data.y_test)
+        dep_length = len(Config.CLASS_COL)
+        for i in range(0,array_length):
+            test_a = self.predictions[i]
+            test_b = data.y_test[i]
+            correct_count = 0
+            print("Instance ",i+1," :")
+            print("Correct labels : ",test_b)
+            print("Predicted labels : ",test_a)
+            for j in range(0,dep_length):
+                if (test_a[j] == test_b[j]):
+                    correct_count = correct_count +1
+                else:
+                    break
+            induvidual_accuracies.append(round((correct_count/dep_length),2))
+            print("Instance Accuracy : ",round((correct_count/dep_length),2))
+        print("***********************************************************")
+        print("Final Model Accuracy : ",round((sum(induvidual_accuracies)/array_length)*100,2)," %")
+        print("***********************************************************")
+
 
 
     def data_transform(self) -> None:
