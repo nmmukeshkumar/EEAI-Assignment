69b13c35fa87d441bb020e42840f3b0162070445
Balaji <balaji0017@gmail.com>
2024-04-25 19:18:43 +0100

    Updated the Random Forest Class to make it a multilabel classification and have also changed the training code

--- model/randomforest.py
+++ model/randomforest.py
@@ -2,12 +2,15 @@ import numpy as np
 import pandas as pd
 from model.base import BaseModel
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.multioutput import MultiOutputClassifier
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'Config')))
+from Config import *
 from numpy import *
 import random
 num_folds = 0
 seed =0
-# Data
+
 np.random.seed(seed)
 random.seed(seed)
 pd.set_option('display.max_rows', 500)
@@ -20,16 +23,18 @@ class RandomForest(BaseModel):
     def __init__(self,
                  model_name: str,
                  embeddings: np.ndarray,
-                 y: np.ndarray) -> None:
+                 y) -> None:
         super(RandomForest, self).__init__()
         self.model_name = model_name
         self.embeddings = embeddings
         self.y = y
-        self.mdl = RandomForestClassifier(n_estimators=1000, random_state=seed, class_weight='balanced_subsample')
+        self.bsemdl = RandomForestClassifier(n_estimators=1000, random_state=seed, class_weight='balanced_subsample')
+        self.mdl = MultiOutputClassifier(self.bsemdl, n_jobs=-1)
         self.predictions = None
         self.data_transform()
 
     def train(self, data) -> None:
+        print("Training")
         self.mdl = self.mdl.fit(data.X_train, data.y_train)
 
     def predict(self, X_test: pd.Series):
