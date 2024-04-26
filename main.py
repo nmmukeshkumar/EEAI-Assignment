b42fd169519a564845ac04298b49352acc2c8148
Balaji <balaji0017@gmail.com>
2024-04-26 21:01:01 +0100

    Modified the main method, and inserted the sketch

--- main.py
+++ main.py
@@ -33,11 +33,13 @@ if __name__ == '__main__':
     df[Config.INTERACTION_CONTENT] = df[Config.INTERACTION_CONTENT].values.astype('U')
     df[Config.TICKET_SUMMARY] = df[Config.TICKET_SUMMARY].values.astype('U')
     grouped_df = df.groupby(Config.GROUPED)
-    i = 0
+    group=['Design Decision 1: Chained Multi-outputs','Design Decision 2: Hierarchical Modelling']
+    i=1
     for name, group_df in grouped_df:
-        print("Group ",i+1," :")
+        print(f"Group - {group[i-1]}:")
         X, group_df = get_embeddings(group_df)
         data = get_data_object(X, group_df)
         perform_modelling(data, group_df, name)
         i = i+1
+        print(150*'*')
 
